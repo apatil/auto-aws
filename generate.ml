c6bbd1d3adc6ba9619ea58c2ec8c1792fa59e874
Anand Patil <anand.prabhakar.patil@gmail.com>
2014-12-28 10:57:33 -0800

    Added default cases

--- generate.ml
+++ generate.ml
@@ -17,14 +17,14 @@ let rec transformList _loc shapeName =
   let shape = shapes |> member shapeName in
   let uncapShapeName = String.uncapitalize shapeName in
   match shapes |> member shapeName |> member "type" |> to_string with
-      "list" -> (
+    | "list" -> (
         let memberTyp = shape |> member "member" |> member "shape" |> to_string |> transformBasicSubShape _loc in
         (* Is there a way to do this with qusoations? *)
         Ast.TyApp (_loc, <:ctyp<list>>, memberTyp))
     | _ -> <:ctyp<$uid:uncapShapeName$>>
 and transformBasicSubShape _loc shapeName = 
   match shapeName with
-      "Boolean" -> <:ctyp<bool>>
+    | "Boolean" -> <:ctyp<bool>>
     | "Integer" | "Long" -> <:ctyp<int>>
     | "Float" | "Double" -> <:ctyp<float>>
     (* TODO: What we do with blob? *)
@@ -33,14 +33,13 @@ and transformBasicSubShape _loc shapeName =
     | "String" -> <:ctyp<string>>
     | _ -> transformList _loc shapeName
 
-(* TODO: Equip with an XML transformer. *)
 let shape2Record _loc (name,shape) = 
-  let shape2EntryList _loc (name, field) =
+  let shape2Field _loc (name, field) =
     let subShape = transformBasicSubShape _loc @@ to_string @@ member "shape" field in
     (_loc, (String.uncapitalize name), false, subShape)
   in
   
-  let entries = List.map (shape2EntryList _loc) (shape |> member "members" |> to_assoc) in
+  let entries = List.map (shape2Field _loc) (shape |> member "members" |> to_assoc) in
   (* TODO: Apply locationName here. *)
   (* TODO: Handle optional records here. *)
   let typeDef = Ast.record_type_of_list entries in
@@ -57,7 +56,6 @@ let canBeVariant name =
   Str.string_match testRe name 0
 let allCanBeVariants enum =
   List.for_all (fun el -> el |> to_string |> canBeVariant) (to_list enum)
-
 
 let shape2Variant _loc (name, shape) =
   let name2Variant name =
@@ -86,13 +84,14 @@ let shape2Variant _loc (name, shape) =
   in
   let createDeclaration name decl = <:str_item<type $lid:name$ = $list:decl$>> 
   in
+  (* TODO: This should be createStringOf and so on, the names are mixed up. *)
   let createOfString name os =
-    let osName = Printf.sprintf "%s_of_string" name in
+    let osName = Printf.sprintf "string_of_%s" name in
     <:str_item<let $lid:osName$ x = match x with $list:os$>>
   in
   let createStringOf name so =
-    let defaultCase = (Ast.McArr (_loc, (Ast.PaId (_loc, (Ast.IdUid (_loc, "other")))), (Ast.ExNil _loc), <:expr<invalid_arg Printf.sprintf "No variant of type %s corresponds to received value %s" name other>>)) in
-    let soName = Printf.sprintf "string_of_%s" name in
+    let defaultCase = (Ast.McArr (_loc, (Ast.PaId (_loc, (Ast.IdUid (_loc, "other")))), (Ast.ExNil _loc), <:expr<invalid_arg @@ Printf.sprintf "No variant of type %s corresponds to received value %s" name other>>)) in
+    let soName = Printf.sprintf "%s_of_string" name in
     let soWithDefault = so @ [defaultCase] in
     <:str_item<let $lid:soName$ x = match x with $list:soWithDefault$>>
   in
@@ -107,18 +106,18 @@ let shape2Variant _loc (name, shape) =
 let shape2Compound _loc (name, shape) =
   let t = shape |> member "type" |> to_string in
   let transformer = match t with
-      "structure" -> shape2Record
+    | "structure" -> shape2Record
     | "string" -> shape2Variant
     | _ -> failwith @@ Printf.sprintf "Shape %s has unknown type %s" name t in
   transformer _loc (String.uncapitalize name, shape)
 
 let isCompound (name, shape) = 
   match shape |> member "type" |> to_string with
-      "structure" -> true
+    | "structure" -> true
     | "string" -> (
       let enum = shape |> member "enum" in 
       match enum with
-          `Null -> false
+        | `Null -> false
         | `List x when allCanBeVariants enum -> true
         | `List x -> false
         | _ -> failwith @@ Printf.sprintf "Shape %s has non-list enum" name)
@@ -127,7 +126,7 @@ let isCompound (name, shape) =
 
 let getMemberDocs (name, el) =
   match el |> member "documentation" with
-      `String x -> Printf.sprintf "\n  <li> %s: %s <\\li>" name x
+    | `String x -> Printf.sprintf "\n  <li> %s: %s <\\li>" name x
     | `Null -> ""
     | _ -> failwith "Unrecognized member documentation type."
 let getDocs (name, shape)  =
@@ -136,7 +135,7 @@ let getDocs (name, shape)  =
     else "" in
   (* Printf.printf "%s\n" name; *)
   match shape |> member "documentation" with
-      `String x -> Printf.sprintf "\n(**\n%s%s\n**)\n" x recordDocs
+    | `String x -> Printf.sprintf "\n(**\n%s%s\n**)\n" x recordDocs
     | `Null -> "\n"
     | _ -> failwith "Unrecognized documentation type."
 
