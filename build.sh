bf233dab5eaf9ef56b2c6c3a014b6c487ed9a974
Anand Patil <anand.prabhakar.patil@gmail.com>
2014-12-28 10:43:06 -0800

    Generating to_string and of_string for variants

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 # ocamlfind ocamlopt -package yojson -c test.ml
 # ocamlfind ocamlopt -package yojson test.cmx -linkpkg -o test
-
-ocamlfind ocamlc -g -pp camlp4of -I +camlp4 -package camlp4.quotations.o -package camlp4.lib -package yojson -package core -package str -thread -linkpkg generate.ml -o generate
-
+rm generated.ml || true
+ocamlfind ocamlc -g -pp camlp4of -I +camlp4 -package camlp4.quotations.o -package camlp4.lib -package yojson -package core -package str -thread -linkpkg generate.ml -o generate && echo "success"; ./generate > generated.ml
 # ocamlfind ocamlopt -pp camlp4of -I +camlp4 -package camlp4.quotations.o -package camlp4.lib -package yojson -linkpkg -o test
--- generate.ml
+++ generate.ml
@@ -7,93 +7,114 @@ let (%) f g = function (x) -> f (g x)
 
 let fname = "/home/anand/auto_aws/botocore/botocore/data/aws/ec2/2014-09-01.api.json"
 let signature = Yojson.Basic.from_file fname
-let notImplemented = 
-  let _loc = Loc.ghost in
-    <:str_item<failwith "Not Implemented">>
+let shapes = signature |> member "shapes"
+(* let docs = member "documentation" signature in *)
+(* let meta = member "metadata" signature in *)
+(* This will produce a list of (string * json) tuples *)
+(* let oper = signature |> member "operations" |> to_assoc in *)
 
-let transformBasicSubShape _loc shapeName = 
+let rec transformList _loc shapeName =
+  let shape = shapes |> member shapeName in
+  let uncapShapeName = String.uncapitalize shapeName in
+  match shapes |> member shapeName |> member "type" |> to_string with
+      "list" -> (
+        let memberTyp = shape |> member "member" |> member "shape" |> to_string |> transformBasicSubShape _loc in
+        (* Is there a way to do this with qusoations? *)
+        Ast.TyApp (_loc, <:ctyp<list>>, memberTyp))
+    | _ -> <:ctyp<$uid:uncapShapeName$>>
+and transformBasicSubShape _loc shapeName = 
   match shapeName with
-      "boolean" -> <:ctyp<bool>>
-    | "integer" | "long" -> <:ctyp<int>>
-    | "float" | "double" -> <:ctyp<float>>
-    (* FIXME: What we do with blob. *)
-    | "blob" -> <:ctyp<blob>>
-    | "timestamp" | "dateTime" -> <:ctyp<Date.t>>
-    | "string" -> <:ctyp<string>>
-    | _ -> <:ctyp<$uid:shapeName$>>
+      "Boolean" -> <:ctyp<bool>>
+    | "Integer" | "Long" -> <:ctyp<int>>
+    | "Float" | "Double" -> <:ctyp<float>>
+    (* TODO: What we do with blob? *)
+    | "Blob" -> <:ctyp<blob>>
+    | "Timestamp" | "DateTime" -> <:ctyp<Date.t>>
+    | "String" -> <:ctyp<string>>
+    | _ -> transformList _loc shapeName
 
 (* TODO: Equip with an XML transformer. *)
 let shape2Record _loc (name,shape) = 
-
   let shape2EntryList _loc (name, field) =
-    let subShape = transformBasicSubShape _loc @@ String.uncapitalize @@ to_string @@ member "shape" field in 
+    let subShape = transformBasicSubShape _loc @@ to_string @@ member "shape" field in
     (_loc, (String.uncapitalize name), false, subShape)
   in
   
   let entries = List.map (shape2EntryList _loc) (shape |> member "members" |> to_assoc) in
-  (* TODO: Would like to pipe the first argument through Ast.TyRec and Ast.TyDcl. Can this kind of thing be done without first-class variant constructors? *)
   (* TODO: Apply locationName here. *)
   (* TODO: Handle optional records here. *)
-  let typeDef = Ast.TyRec (_loc, Ast.record_type_of_list entries) in
+  let typeDef = Ast.record_type_of_list entries in
   (* TODO: Equip with an XML transformer. *)
-  <:str_item<type $lid:name$ = $typeDef$>>
+  <:str_item<type $lid:name$ = {$typeDef$}>>
 
-(* We try to make AWS enum strings into variant types, but some enums are not legal OCaml identifiers. Examples:
-  * not-applicable : We translate hyphens to underscores, which are not used by ec2
-  * t1.micro : We translate dots to single quotes, which are also not used by ec2 
-  * Linux/UNIX (Amazon VPC) : We give up and type the field as a string, not an enum. 
+(* We try to make AWS enum strings into variant types, but some enums are nso legal OCaml identifiers. Examples:
+  * nso-applicable : We translate hyphens to camel casing.
+  * t1.micro : We translate dsos to underscores.
+  * Linux/UNIX (Amazon VPC) : We give up and type the field as a string, nso an enum. 
 These functions let us distinguish which enums can be variant types.*)
 let canBeVariant name = 
-  let testRe = Str.regexp "^[a-zA-Z0-9\\.-]+$" in
+  let testRe = Str.regexp "^[a-zA-Z0-9\\.-\\ ]+$" in
   Str.string_match testRe name 0
 let allCanBeVariants enum =
   List.for_all (fun el -> el |> to_string |> canBeVariant) (to_list enum)
-let name2Variant name =
-  let hyphenToCamel name =
-    String.concat "" @@ List.map String.capitalize @@ Str.split (Str.regexp "-") name
-  in
-  let dotToUnderscore name =
-    Str.global_replace (Str.regexp "\\.") "_" name
-  in
-  String.capitalize @@ dotToUnderscore @@ hyphenToCamel name
 
-(* TODO: Use the variantslib syntax transformer on enum types, and equip with an xml transformer. *)
+
 let shape2Variant _loc (name, shape) =
-  let getVariant el = 
-    let s = name2Variant @@ to_string el in
-    <:ctyp<$uid:s$ >>
-  in 
-  
-  let values = List.map getVariant (shape |> member "enum" |> to_list) in
-  <:str_item<type $lid:name$ = $list:values$>>
-
-(* TODO: Equip with an XML transformer. *)
-let shape2List _loc (name, shape) =
-  let memberDef = shape |> member "member" in
-  let memberTyp = memberDef |> member "shape" |> to_string |> String.uncapitalize |> transformBasicSubShape _loc in
-  (* Is there a way to do this with all quotations? *)
-  let typ = Ast.TyApp (_loc, <:ctyp<list>>, memberTyp) in
-  let out = <:str_item<type $lid:name$ = $typ$>> in
-  (* The return value is computed, but validate locationName to be sure we haven't missed anything. *)
-  match memberDef |> member "locationName" with
-      `String "item" -> out
-    (* If locationName is anything else, this can be a single value. But, for simplicity, we don't support the single value case. *)
-    | `String x -> out
-    | `Null -> out
-    | _ -> failwith @@ Printf.sprintf "locationName was not string or null for %s" name
+  let name2Variant name =
+    let toCamel sep =
+      fun (name) -> String.concat "" @@ List.map String.capitalize @@ Str.split (Str.regexp sep) name
+    in
+    String.capitalize @@ (toCamel "\\.") @@ (toCamel "-") @@ (toCamel "\\ ") name
+  in
+  let ofStringMatch constructor str =
+    (Ast.McArr (_loc, (Ast.PaId (_loc, (Ast.IdUid (_loc, constructor)))), (Ast.ExNil _loc), (Ast.ExStr (_loc, str))))
+  in
+  let stringOfMatch str constructor =
+    (Ast.McArr (_loc, (Ast.PaStr (_loc, str)), (Ast.ExNil _loc), (Ast.ExId (_loc, (Ast.IdUid (_loc, constructor))))))
+  in
+  let declLine constructor =
+    <:ctyp<$uid:constructor$ >>
+  in
+  let getVariant (decl, so, os) el = 
+    let origName = to_string el in
+    let constructor = name2Variant @@ origName in
+    (
+      (declLine constructor) :: decl,
+      (stringOfMatch origName constructor) :: so,
+      (ofStringMatch constructor origName) :: os
+    )
+  in
+  let createDeclaration name decl = <:str_item<type $lid:name$ = $list:decl$>> 
+  in
+  let createOfString name os =
+    let osName = Printf.sprintf "%s_of_string" name in
+    <:str_item<let $lid:osName$ x = match x with $list:os$>>
+  in
+  let createStringOf name so =
+    let defaultCase = (Ast.McArr (_loc, (Ast.PaId (_loc, (Ast.IdUid (_loc, "other")))), (Ast.ExNil _loc), <:expr<invalid_arg Printf.sprintf "No variant of type %s corresponds to received value %s" name other>>)) in
+    let soName = Printf.sprintf "string_of_%s" name in
+    let soWithDefault = so @ [defaultCase] in
+    <:str_item<let $lid:soName$ x = match x with $list:soWithDefault$>>
+  in
+  let (decl, so, os) = List.fold_left getVariant ([], [], []) (shape |> member "enum" |> to_list) in
+  let code = [
+    createDeclaration name decl;
+    createOfString name os;
+    createStringOf name so;
+  ] in
+  <:str_item<$list:code$>>
 
 let shape2Compound _loc (name, shape) =
   let t = shape |> member "type" |> to_string in
   let transformer = match t with
       "structure" -> shape2Record
-    | "list" -> shape2List
     | "string" -> shape2Variant
     | _ -> failwith @@ Printf.sprintf "Shape %s has unknown type %s" name t in
   transformer _loc (String.uncapitalize name, shape)
 
 let isCompound (name, shape) = 
   match shape |> member "type" |> to_string with
-      "structure" | "list" -> true
+      "structure" -> true
     | "string" -> (
       let enum = shape |> member "enum" in 
       match enum with
@@ -101,13 +122,8 @@ let isCompound (name, shape) =
         | `List x when allCanBeVariants enum -> true
         | `List x -> false
         | _ -> failwith @@ Printf.sprintf "Shape %s has non-list enum" name)
+    (* We don't generate separate types for lists. *)
     | _ -> false 
-
-(* let docs = member "documentation" signature in *)
-(* let meta = member "metadata" signature in *)
-(* This will produce a list of (string * json) tuples *)
-(* let oper = signature |> member "operations" |> to_assoc in *)
-let shapes = signature |> member "shapes" |> to_assoc |> (List.filter isCompound)
 
 let getMemberDocs (name, el) =
   match el |> member "documentation" with
@@ -133,8 +149,7 @@ let printModuleDocs signature =
   Printf.printf "\n(**\n%s\n**)\n" (signature |> member "documentation" |> to_string)
 ;;
 
-(* let typeNodes = List.map (shape2Compound Loc.ghost) *)
-
 printModuleDocs signature;;
-List.iter generateShapeWithDocs shapes
+List.iter generateShapeWithDocs (shapes |> to_assoc |> (List.filter isCompound))
+
 
